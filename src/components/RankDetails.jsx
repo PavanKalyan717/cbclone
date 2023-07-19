48c1e46345736b38c7db0340575da9a77063575e
Pavan Kalyan <pavankalyanm23@gmail.com>
2023-07-19 17:17:59 +0530

    Ranks are now displaying as per Categories

--- src/components/RankDetails.jsx
+++ src/components/RankDetails.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import statinfo from '../constants/stats'
 
-const RankDetails = () => {
+const RankDetails = ({selected}) => {
+  // const [category, setCategory] = useState('')
+  // const [format, setFormat] = useState('')
+  // const [gender, setGender] = useState('')
+  
+  // if(statinfo.Category.includes(selected)){
+  //   setCategory(selected)
+  // }
+  // else if(statinfo.formatType.includes(selected)){
+  //   setFormat(selected)
+  // }
+  // else{
+  //   setGender(selected)
+  // }
+  // console.log('Search array is', category,format,gender)
+  
+  useEffect(() => {
+   
+  }, [])
+  
   return (
-    <div>RankDetails</div>
+    <div>
+      {
+        selected
+      }
+    </div>
   )
 }
 
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,106 +1,222 @@
-import React,{useState,Fragment,useEffect} from 'react'
-import { Listbox,Transition } from '@headlessui/react'
-import ChevRonImg from '../assets/chevron-up-down.svg'
+//import React,{useState,Fragment,useEffect} from 'react'
+// import { Listbox,Transition } from '@headlessui/react'
+// import ChevRonImg from '../assets/chevron-up-down.svg'
 import statinfo from '../constants/stats'
 import { fetchRankFromAPI } from '../utils/fetchFromAPI'
-import RankDetails from './RankDetails'
+// import RankDetails from './RankDetails'
+import ranks from '../constants/ranks'
 
-const StatsInfoMenu = ({options}) => {
-    const [selected, setSelected] = useState(options[0])
+// const StatsInfoMenu = ({options}) => {
+//     const [selected, setSelected] = useState(options[0])
+//     const [rankData, setRankData] = useState([])
+//     console.log('selected is',selected)
+//     console.log('rank Data is',rankData)
+
+
+//     // useEffect(() => {
+//     //     const getRank= ['batsmen','test','Men']
+
+//     //   if(statinfo.Category.includes(selected)){
+//     //     getRank[0]=selected
+//     //   }
+//     //   else if(statinfo.formatType.includes(selected)){
+//     //     getRank[1]=selected
+//     //   }
+//     //   console.log(getRank)
+//     //   fetchRankFromAPI(`stats/v1/rankings/${getRank[0]}?formatType=${getRank[1]}`).then((data)=>
+//     //   (setRankData(data)))
+//     // },[selected])
+
+
+//     return (
+//         <div className='w-fit'>
+//             <Listbox
+//                 value={selected}
+//                 onChange={(e) => {
+//                     setSelected(e)
+//                 }}
+
+//             >
+//                 <div className='relative w-fit z-10'>
+//                     <Listbox.Button className='relative w-full min-w-[147px] flex
+//                      justify-between items-center cursor-pointer rounded-lg bg-white
+//                       py-2 px-3 text-left shadow-md sm:text-sm border'>
+//                         <span className='block truncate'>{selected }</span>
+//                         < img
+//                             src={ChevRonImg}
+//                             width={20}
+//                             height={20}
+//                             className='ml-4 object-contain'
+//                             alt='chevron up down'
+//                         />
+//                     </Listbox.Button>
+//                     <Transition
+//                         as={Fragment}
+//                         leave='transition ease-in duration-100'
+//                         leaveFrom='opactiy-100'
+//                         leaveTo='opacity-0'
+//                     >
+//                         <Listbox.Options
+//                             className='absolute mt-1 max-h-60 w-full 
+//                             overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1
+//                              ring-black ring-opacity-5 focus:outline-none sm:text-sm;'
+//                         >
+//                             {
+//                                 options.map((option) => (
+//                                     <Listbox.Option
+//                                         key={option}
+//                                         value={option}
+//                                         className={({ active }) => `relative cursor-pointer select-none py-2 px-4
+//                         ${active ? 'bg-[#28bc95]' : 'text-gray-900'}
+//                         `}
+//                                     >
+//                                         {({ selected }) => (
+//                                             <span className={`block truncate  ${selected ? "font-medium" : "font-normal"}`}>
+//                                                 {option}
+//                                             </span>
+//                                         )
+//                                         }
+//                                     </Listbox.Option>
+//                                 ))
+//                             }
+//                         </Listbox.Options>
+//                     </Transition>
+//                 </div>
+//             </Listbox>
+//             <RankDetails selected={selected}/>
+//         </div>
+//     )
+// }
+
+// const Stats=()=>{
+
+//     return(
+//         <div className='flex justify-center' >
+//             {
+//                 Object.keys(statinfo).map((key)=>(
+//                    <div className='px-4'>
+//                        <StatsInfoMenu options={statinfo[key]}  />
+//                     </div> 
+//                 ))
+//             }
+//         </div>
+//     )
+// }
+
+
+// export default Stats
+
+
+
+
+import React, { useState } from 'react'
+
+const Stats = () => {
+    const [category, setCategory] = useState('batsmen')
+    const [format, setFormat] = useState('odi')
+    const [gender, setGender] = useState('Men')
     const [rankData, setRankData] = useState([])
-    console.log('selected is',selected)
-    console.log('rank Data is',rankData)
+    // const [isSubmitted, setisSubmitted] = useState(false)
 
+    const Trend=({trend})=>{
+        if(trend==='Flat')
+        {
+            return(<>{'-'}</>)
+        }
+        else if(trend==='Up')
+        {
+            return(<>{'▲'}</>)
+        }
+        else{
+            return(<>{'▼'}</>)
+        }
 
-    useEffect(() => {
-        const getRank= ['batsmen','test','Men']
+    }
 
-      if(statinfo.Category.includes(selected)){
-        getRank[0]=selected
-      }
-      else if(statinfo.formatType.includes(selected)){
-        getRank[1]=selected
-      }
-      console.log(getRank)
-      fetchRankFromAPI(`stats/v1/rankings/${getRank[0]}?formatType=${getRank[1]}`).then((data)=>
-      (setRankData(data)))
-    },[selected])
-    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(category, format, gender)
+        fetchRankFromAPI(`stats/v1/rankings/${category}?formatType=${format}`).then(
+            (data) => {
+                setRankData(data)
+                console.log(rankData)
+            }
+        )
+    }
+
+    const handleCategory = (e) => {
+        setCategory(e.target.value)
+    }
+    const handleFormat = (e) => {
+        setFormat(e.target.value)
+    }
+    const handleGender = (e) => {
+        setGender(e.target.value)
+    }
 
     return (
-        <div className='w-fit'>
-            <Listbox
-                value={selected}
-                onChange={(e) => {
-                    setSelected(e)
-                }}
+        <div>
+            <form onSubmit={handleSubmit} className='flex flex-row justify-center py-2'>
+                <select value={category} onChange={handleCategory} className='p-2 rounded-lg mx-4' >
+                    {
+                        statinfo.Category.map((item) => (
+                            <option value={item} className='py-2'>
+                                {item}
+                            </option>
+                        ))
+                    }
+                </select>
 
-            >
-                <div className='relative w-fit z-10'>
-                    <Listbox.Button className='relative w-full min-w-[147px] flex
-                     justify-between items-center cursor-pointer rounded-lg bg-white
-                      py-2 px-3 text-left shadow-md sm:text-sm border'>
-                        <span className='block truncate'>{selected }</span>
-                        < img
-                            src={ChevRonImg}
-                            width={20}
-                            height={20}
-                            className='ml-4 object-contain'
-                            alt='chevron up down'
-                        />
-                    </Listbox.Button>
-                    <Transition
-                        as={Fragment}
-                        leave='transition ease-in duration-100'
-                        leaveFrom='opactiy-100'
-                        leaveTo='opacity-0'
-                    >
-                        <Listbox.Options
-                            className='absolute mt-1 max-h-60 w-full 
-                            overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1
-                             ring-black ring-opacity-5 focus:outline-none sm:text-sm;'
-                        >
-                            {
-                                options.map((option) => (
-                                    <Listbox.Option
-                                        key={option}
-                                        value={option}
-                                        className={({ active }) => `relative cursor-pointer select-none py-2 px-4
-                        ${active ? 'bg-[#28bc95]' : 'text-gray-900'}
-                        `}
-                                    >
-                                        {({ selected }) => (
-                                            <span className={`block truncate  ${selected ? "font-medium" : "font-normal"}`}>
-                                                {option}
-                                            </span>
-                                        )
-                                        }
-                                    </Listbox.Option>
-                                ))
-                            }
-                        </Listbox.Options>
-                    </Transition>
-                </div>
-            </Listbox>
-            <RankDetails />
+                <select value={format} onChange={handleFormat} className='p-2 rounded-lg mx-4'>
+                    {
+                        statinfo.formatType.map((item) => (
+                            <option value={item}>
+                                {item}
+                            </option>
+                        ))
+                    }
+                </select>
+                <select value={gender} onChange={handleGender} className='p-2 rounded-lg mx-4' >
+                    {
+                        statinfo.Gender.map((item) => (
+                            <option value={item}>
+                                {item}
+                            </option>
+                        ))
+                    }
+                </select>
+                <input type='submit' value='Submit' className='bg-slate-600 cursor-pointer p-2 rounded-lg mx-4' />
+            </form>
+            <div>
+                <table className='w-full text-center'>
+                    <tr >
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Country</th>
+                        <th>Rating</th>
+                        <th>Points</th>
+                        <th>trend</th>
+                        <th>profile Pic</th>
+                    </tr>
+                    
+
+                        {
+                            ranks.rank.map((item) => (
+                            <tr >    
+                                <td>{item.rank}</td>
+                                <td>{item.name}</td>
+                                <td>{item.country}</td>
+                                <td>{item.rating}</td>
+                                <td>{item.points}</td>
+                                <td><Trend trend={item.trend}/></td>
+                                <td>{item.faceImageId}</td>
+                            </tr>
+                            ))
+                        } 
+                </table>
+            </div>
         </div>
     )
 }
-
-const Stats=()=>{
-
-    return(
-        <div className='flex justify-center' >
-            {
-                Object.keys(statinfo).map((key)=>(
-                   <div className='px-4'>
-                       <StatsInfoMenu options={statinfo[key]}  />
-                    </div> 
-                ))
-            }
-        </div>
-    )
-}
-
 
 export default Stats

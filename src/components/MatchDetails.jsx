a852196fd0132541d839f33d0b6ad05c344688be
Pavan Kalyan <pavankalyanm23@gmail.com>
2023-07-21 17:59:08 +0530

    added Design for match Card

--- src/components/MatchDetails.jsx
+++ src/components/MatchDetails.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Carousel from 'react-grid-carousel'
 import { useLocation } from 'react-router-dom'
 
 const MatchDetails = () => {
@@ -6,34 +7,48 @@ const MatchDetails = () => {
     const details = location.state
     console.log('match details are', details)
   return (
-    <div className='grid grid-cols-4 grid-flow-row'>
+    <div  className='py-6' >
+        <Carousel cols={4} rows={1}>
+            
         {
             details?.matches?.map((match)=>(
-                <div className='p-5'>
-                    <p>{match?.matchInfo?.matchFormat}</p>
-                    <p>{match?.matchInfo?.seriesName}</p>
-                    <p>{match?.matchInfo?.status}</p>
-                    <p>{match?.matchInfo?.team1?.teamSName} vs {match?.matchInfo?.team2?.teamSName}</p>           
+                <Carousel.Item>
+                <div className='p-5 bg-white h-[12rem] w-[20rem] rounded-lg shadow-lg cursor-pointer '>
+                    <p className='text-[12px]'>{match?.matchInfo?.seriesName}</p>
+                    <div className='flex justify-between items-center py-2'>
+                        <><p>{match?.matchInfo?.matchDesc}</p>
+                        </>
+                        <>
+                        <p className='rounded-full bg-red-700 text-[12px] px-2 text-white font-semibold'>
+                            {match?.matchInfo?.matchFormat}
+                        </p>
+                        </>
+                    </div>
+                    {/* <p>{match?.matchInfo?.team1?.teamSName} vs {match?.matchInfo?.team2?.teamSName}</p>     */}
                     <div>
-                        <p>Match Score:</p>
-                        <div className='flex flex-row justify-between'>
+                        
+                        <div className='flex flex-row justify-between text-sm'>
                             <p>{match?.matchInfo?.team1?.teamSName}</p>
                             <p>
                             {match?.matchScore?.team1Score.inngs1?.runs}/{match?.matchScore?.team1Score.inngs1?.wickets}
                             <span> </span>
                             ({match?.matchScore?.team1Score.inngs1?.overs})</p>
                         </div>
-                        <div className='flex flex-row justify-between'>
+                        <div className='flex flex-row justify-between text-sm '>
                             <p>{match?.matchInfo?.team2?.teamSName}</p>
                             <p>
                             {match?.matchScore?.team2Score.inngs1?.runs}/{match?.matchScore?.team2Score.inngs1?.wickets}
                             <span> </span>
                             ({match?.matchScore?.team2Score.inngs1?.overs})</p>
                         </div>
-                    </div>       
+                    </div>
+                    <p className='text-[14px] py-2'>{match?.matchInfo?.status}</p>
+                           
                 </div>
+     </Carousel.Item>
             ))
         }
+        </Carousel>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <nav className='flex flex-row justify-around items-center
-    py-2 font-extrabold text-xl bg-[#009270]'>
+    py-2 font-extrabold text-xl bg-[#009270] shadow-lg'>
       <div className='flex flex-row items-center'>
       <Link to='/'>
         <img src={CBImage} alt="logo" width={60} height={60}
